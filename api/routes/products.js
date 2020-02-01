116c57499e617406a1644655b89ac35396763cce
Elon Aseneka <elonsantos63@gmail.com>
2020-02-01 10:39:55 +0300

    fix(routes): fix products route

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,16 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const productsControllers = require('../controllers/products');
-const path = require('path');
-const multer = require('multer');
-const upload = multer({dest: path.resolve('images/products')});
+const productsControllers = require("../controllers/products");
+const path = require("path");
+const multer = require("multer");
+const generateUUID = require("uuid/v4");
 
-router.get('/', productsControllers.getProducts);
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.resolve("public/images/products"));
+  },
+  filename: (req, file, cb) => {
+    let randName = generateUUID();
+    console.log('name', randName);
+    
+    let mimetype = file.mimetype;
+    let begin = mimetype.lastIndexOf("/") + 1;
+    let extension = mimetype.substr(begin);
+    let file_name = `${randName}.${extension}`;
+    cb(null, file_name);
+  }
+});
 
-router.post('/add', upload.single('file'), productsControllers.addProduct);
+const upload = multer({ storage });
 
-router.patch('/edit/:id', productsControllers.editProduct);
+router.get("/", productsControllers.getProducts);
 
-router.delete('/delete/:id', productsControllers.deleteProduct);
+router.post(
+  "/add",
+  upload.array("productFiles", 10),
+  productsControllers.addProduct
+);
+
+router.patch("/edit/:id", productsControllers.editProduct);
+
+router.delete("/delete/:id", productsControllers.deleteProduct);
 
 module.exports = router;
